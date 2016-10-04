c717675f089b1bb0ba0385529110da8c4cb80daf
Jesse L. Zamora <xtremekforever@gmail.com>
2016-10-04 13:45:02 -0700

    Benchmark performance of parsing reports using BinData from tests

--- test/gprs_test.rb
+++ test/gprs_test.rb
@@ -1,12 +1,53 @@
 require 'test_helper'
 
+require 'benchmark'
+
 class GprsTest < Minitest::Test
+  # TODO: Remove this later?
+  def self.test_order
+   :alpha
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Gprs::VERSION
   end
 
-  def test_parse_report_single
-    # Unit ID: 2113
-    puts Gprs.parse_report([0x28,0xB1,0x68,0x66,0x2C,0x93,0x6D,0x1F,0x41,0x10,0x88,0x45,0xA0,0x3B,0x54,0x69,0x6E,0x1,0x3,0x1F,0x9B,0x2,0x6F,0xA4,0x1,0x85,0x5,0x0,0x0,0x0,0x2,0x7A,0x29,0xD])
+  def test_parse_report_1
+    parse_report_single(true)
+  end
+
+  def test_parse_report_5
+    parse_report_multiple(true)
+  end
+
+  def test_parse_report_bench
+    puts ""
+    puts "# Benchmarking report parsing performance:"
+
+    Benchmark.bm(40) do |x|
+      x.report("5000 packets (1 report 5000 times)") do
+        for i in 0..5000
+          parse_report_single(false)
+        end
+      end
+
+      x.report("5000 packets (5 reports 1000 times)") do
+        for i in 0..1000
+          parse_report_multiple(false)
+        end
+      end
+
+      x.report("10000 packets (1 report 10000 times)") do
+        for i in 0..10000
+          parse_report_single(false)
+        end
+      end
+
+      x.report("100000 packets (1 report 100000 times)") do
+        for i in 0..100000
+          parse_report_single(false)
+        end
+      end
+    end
   end
 end
