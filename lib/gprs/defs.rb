4273fca6f0b81018f0d4d3e1387018f4b13706e5
Jesse L. Zamora <xtremekforever@gmail.com>
2016-10-04 11:00:36 -0700

    Namespace and cleanup gprs/report parsing

--- lib/gprs/defs.rb
+++ lib/gprs/defs.rb
@@ -1,21 +1,91 @@
 
 require "bindata"
 
-# Common GPRS fields
-GPRS_SOP        = 0x28
-GPRS_EOP        = 0x29
-GPRS_NEWLINE    = 0x0D
-GPRS_DLE        = 0x10
-GPRS_DLE_OFFSET = 128
+module Gprs
+  # Common GPRS fields
+  GPRS_SOP        = 0x28
+  GPRS_EOP        = 0x29
 
-class ZeroSkip < BinData::Primitive
-  skip :length => 0
+  GPRS_BACKSPACE  = 0x8
+  GPRS_CR         = 0xA
+  GPRS_NL         = 0xD
 
-  def get; 0; end
-  def set(v); ; end
-end
+  GPRS_DLE        = 0x10
+  GPRS_DLE_OFFSET = 128
 
-class UInt8If < BinData::Choice
-  zero_skip   0
-  uint8       1
+  # Default definition for skipping a not-present value (read length of 0)
+  class SkipNull < BinData::Primitive
+    skip :length => 0
+
+    def get; 0; end
+    def set(v); ; end
+  end
+
+  class UInt8If < BinData::Choice
+    skip_null   0
+    uint8       1
+  end
+
+  class Defs
+    # Returns true if the byte needs to be escaped
+    def self.needs_escape?(byte)
+      [ GPRS_SOP, GPRS_EOP, GPRS_DLE, GPRS_BACKSPACE, GPRS_CR, GPRS_NL, GPRS_DLE ].contains(byte)
+    end
+
+    # Unescapes all escaped bytes in the packet
+    def self.unescape(packet)
+      escape_next = false
+      packet.map! { |byte|
+        if byte == GPRS_DLE
+          # -1 to mark byte for removal
+          escape_next = true; byte = -1
+        elsif escape_next
+          # Escape this byte
+          escape_next = false; byte = byte - 128
+        end
+
+        byte
+      }.delete(-1); packet
+    end
+
+    # Calculates CRC from all bytes
+    def self.crc8(bytes)
+      crc = 0; bytes.each { |byte| crc ^= byte }; crc
+    end
+
+    # Takes a raw packet and performs the following:
+    #  - Validate packet format
+    #  - Unescape special bytes
+    #  - Validate CRC
+    #  - Remove protocol bytes (SOP, EOP, CRC, etc)
+    def self.preprocess(packet)
+      # Start by unpacking (if needed)
+      if packet.is_a? String
+        packet = packet.bytes
+      end
+
+      # Strip out last newline (if present)
+      packet.delete(GPRS_NL)
+
+      # Strip out SOP and EOP, return error if invalid
+      if packet.delete(GPRS_SOP).nil? or packet.delete(GPRS_EOP).nil?
+        puts "Invalid Packet: Missing SOP or EOP byte."
+        return false, packet
+      end
+
+      # Strip out escapes
+      packet = unescape(packet)
+
+      # Strip out & validate CRC
+      crc = packet.delete_at(packet.length - 1)
+      ccrc = crc8(packet)
+      if crc != ccrc
+        puts "Invalid CRC: #{crc.to_s} != #{ccrc.to_s}."
+        return false, packet
+      end
+
+      # This method always returns an array of bytes
+      return true, packet
+    end
+  end
 end
--- lib/gprs/report.rb
+++ lib/gprs/report.rb
@@ -4,95 +4,98 @@ require "bindata"
 require "gprs/defs"
 require "gprs/report_data"
 
-REPORT_TYPE_POSITION            = 0     # Position
-REPORT_TYPE_ALPHANUM_MSG        = 1     # No idea what this is for
-REPORT_TYPE_ALPHANUM_MSG_SPEC   = 2     # No idea what this is for
-REPORT_TYPE_CODED_POS           = 3     # Position with alert
-REPORT_TYPE_QUERY_ANS           = 4     # Answer to query
-REPORT_TYPE_COMP_POS            = 6     # Compressed positions
-REPORT_TYPE_ACKNOWLEDGE         = 7     # ACK to messsage from Gateway
-REPORT_TYPE_EXTENDED_DATA       = 8     # Extended report data with code
-REPORT_TYPE_CODED_COMP_POS      = 9     # Compressed positions with alert
+module Gprs
+  REPORT_TYPE_POSITION            = 0     # Position
+  REPORT_TYPE_ALPHANUM_MSG        = 1     # No idea what this is for
+  REPORT_TYPE_ALPHANUM_MSG_SPEC   = 2     # No idea what this is for
+  REPORT_TYPE_CODED_POS           = 3     # Position with alert
+  REPORT_TYPE_QUERY_ANS           = 4     # Answer to query
+  REPORT_TYPE_COMP_POS            = 6     # Compressed positions
+  REPORT_TYPE_ACKNOWLEDGE         = 7     # ACK to messsage from Gateway
+  REPORT_TYPE_EXTENDED_DATA       = 8     # Extended report data with code
+  REPORT_TYPE_CODED_COMP_POS      = 9     # Compressed positions with alert
 
-# Returns a "raw" parse of a report with no conversions performed
-class Report < BinData::Record
-  endian :little
+  # Returns a "raw" parse of a report with no conversions performed
+  class Report < BinData::Record
+    endian :little
 
-  # Ref
-  bit_le  :ref,         :nbits => 6
-  bit_le  :cell_info,   :nbits => 1
-  bit_le  :gps_info,    :nbits => 1
+    # Ref
+    bit_le  :ref,         :nbits => 6
+    bit_le  :cell_info,   :nbits => 1
+    bit_le  :gps_info,    :nbits => 1
 
-  # Type
-  bit_le  :report_type, :nbits => 4
-  bit_le  :has_modsts,  :nbits => 1
-  bit_le  :has_temp,    :nbits => 1
-  bit_le  :id_len,      :nbits => 2
+    # Type
+    bit_le  :report_type, :nbits => 4
+    bit_le  :has_modsts,  :nbits => 1
+    bit_le  :has_temp,    :nbits => 1
+    bit_le  :id_len,      :nbits => 2
 
-  # IO
-  bit_le  :input_1,     :nbits => 1
-  bit_le  :input_2,     :nbits => 1
-  bit_le  :output_1,    :nbits => 1
-  bit_le  :output_2,    :nbits => 1
-  bit_le  :lat_south,   :nbits => 1
-  bit_le  :lon_west,    :nbits => 1
-  bit_le  :has_cog,     :nbits => 1
-  bit_le  :lac_info,    :nbits => 1
+    # IO
+    bit_le  :input_1,     :nbits => 1
+    bit_le  :input_2,     :nbits => 1
+    bit_le  :output_1,    :nbits => 1
+    bit_le  :output_2,    :nbits => 1
+    bit_le  :lat_south,   :nbits => 1
+    bit_le  :lon_west,    :nbits => 1
+    bit_le  :has_cog,     :nbits => 1
+    bit_le  :lac_info,    :nbits => 1
 
-  # Time
-  uint32  :time_secs
+    # Time
+    uint32  :time_secs
 
-  # Device ID (1 - 4 bytes)
-  choice  :device_id, :selection => :id_len do
-    uint8   0
-    uint16  1
-    uint24  2
-    uint32  3
-  end
+    # Device ID (1 - 4 bytes)
+    choice  :device_id, :selection => :id_len do
+      uint8   0
+      uint16  1
+      uint24  2
+      uint32  3
+    end
 
-  # If gps_info
+    # If gps_info
 
-  # Latitude 
-  choice :lat, :selection => :gps_info do
-    zero_skip 0
-    bit       1, :nbits => 23
-  end
+    # Latitude
+    choice :lat, :selection => :gps_info do
+      skip_null 0
+      bit       1, :nbits => 23
+    end
 
-  # GPS Valid
-  choice :gps_valid, :selection => :gps_info do
-    zero_skip 0
-    bit       1, :nbits => 1
-  end
+    # GPS Valid
+    choice :gps_valid, :selection => :gps_info do
+      skip_null 0
+      bit       1, :nbits => 1
+    end
 
-  # Longitude
-  choice :lon, :selection => :gps_info do
-    zero_skip 0
-    uint24    1
-  end
+    # Longitude
+    choice :lon, :selection => :gps_info do
+      skip_null 0
+      uint24    1
+    end
 
-  # Speed
-  u_int8_if :speed, :selection => :gps_info
-  # End if gps_info
+    # Speed
+    u_int8_if :speed, :selection => :gps_info
+    # End if gps_info
 
-  # COG (If has_cog is set)
-  u_int8_if :cog, :selection => :has_cog
+    # COG (If has_cog is set)
+    u_int8_if :cog, :selection => :has_cog
 
-  # Code (For certain types)
-  choice :code, :selection => :report_type do
-    uint8     REPORT_TYPE_CODED_POS
-    uint8     REPORT_TYPE_EXTENDED_DATA
-    uint8     REPORT_TYPE_CODED_COMP_POS
-    zero_skip :default
-  end
+    # Code (For certain types)
+    choice :code, :selection => :report_type do
+      uint8     REPORT_TYPE_CODED_POS
+      uint8     REPORT_TYPE_EXTENDED_DATA
+      uint8     REPORT_TYPE_CODED_COMP_POS
+      skip_null :default
+    end
 
-  # Modsts
-  u_int8_if :modsts, :selection => :has_modsts
+    # Modsts
+    u_int8_if :modsts, :selection => :has_modsts
 
-  # Temp
-  u_int8_if :temp, :selection => :has_temp
+    # Temp
+    u_int8_if :temp, :selection => :has_temp
 
-  choice :report_data, :selection => lambda { report_type == REPORT_TYPE_EXTENDED_DATA } do
-    zero_skip   false
-    report_data true
+    # Extended Report Data
+    choice :report_data, :selection => :report_type do
+      skip_null   :default
+      report_data REPORT_TYPE_EXTENDED_DATA
+    end
   end
 end
