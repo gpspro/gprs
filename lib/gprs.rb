865aae95a2b6126e0f67b50a7932fd05d6ab3647
Jesse L. Zamora <xtremekforever@gmail.com>
2017-06-29 09:33:06 -0700

    Split out test GPRS data to new file and rename GprsC to Gprs
     - GprsC should refer to the GPRS C Extension

--- lib/gprs.rb
+++ lib/gprs.rb
@@ -7,7 +7,7 @@ require "gprs_c"
 require "require_all"
 require_all "lib/kaitai"
 
-module GprsC
+module Gprs
   # If it's a string with format "0xA 0xB 0xC..." convert to byte array
   def self.process_packet(packet)
     if packet.is_a? String and packet.include? "0x"
@@ -33,70 +33,76 @@ module GprsC
     # Convert to readable format
     packet = process_packet(packet)
 
-    # Process with GPRS c extension
-    processed = GprsC.packet_process_c(packet, log).pack("c*")
+    type = GprsC.packet_type_c(packet, log)
 
-    # Parse with Kaitai Struct
-    cmd = GprsCommand.new(Kaitai::Struct::Stream.new(processed))
+    cmd_hash = {}
 
-    # Generate hash from cmd
-    cmd_hash = { :ref => cmd.ref, :type => cmd.type }
-    if cmd.respond_to? :type_class
-      type_class = cmd.type_class
+    if type == 2
+      # Process with GPRS c extension
+      processed = GprsC.packet_process_c(packet, log).pack("c*")
 
-      cmd_hash[:code] = type_class.code
+      # Parse with Kaitai Struct
+      cmd = GprsCommand.new(Kaitai::Struct::Stream.new(processed))
 
-      if (type_class.respond_to? :data) and not type_class.data.nil?
-        data = type_class.data
+      # Generate hash from cmd
+      cmd_hash[:ref] = cmd.ref
+      cmd_hash[:type] = cmd.type
+      if cmd.respond_to? :type_class
+        type_class = cmd.type_class
 
-        data_hash = {}
-        data.instance_variables.each do |field|
-          key = field.to_s.gsub('@', '')
-          val = data.instance_variable_get field
+        cmd_hash[:code] = type_class.code
 
-          # Recognize _presence_bits and build key + value
-          if key.include? "_presence_bits"
-            type_name = key.gsub("_presence_bits", "")
-            value_name = type_name + "_value_bits"
+        if (type_class.respond_to? :data) and not type_class.data.nil?
+          data = type_class.data
 
-            # Kaitai reads bits backwards so we reverse arrays
-            presence_bits = val.reverse
-            value_bits = data.instance_variable_get("@#{value_name}").reverse
+          data_hash = {}
+          data.instance_variables.each do |field|
+            key = field.to_s.gsub('@', '')
+            val = data.instance_variable_get field
 
-            # Add new keys + values if present
-            idx = 0
-            presence_bits.each do |present|
-              if present
-                # Build key name from type and index
-                key_name = "#{type_name}_#{(idx + 1).to_s}"
+            # Recognize _presence_bits and build key + value
+            if key.include? "_presence_bits"
+              type_name = key.gsub("_presence_bits", "")
+              value_name = type_name + "_value_bits"
 
-                # Add to data hash
-                data_hash[key_name.to_sym] = value_bits[idx]
+              # Kaitai reads bits backwards so we reverse arrays
+              presence_bits = val.reverse
+              value_bits = data.instance_variable_get("@#{value_name}").reverse
+
+              # Add new keys + values if present
+              idx = 0
+              presence_bits.each do |present|
+                if present
+                  # Build key name from type and index
+                  key_name = "#{type_name}_#{(idx + 1).to_s}"
+
+                  # Add to data hash
+                  data_hash[key_name.to_sym] = value_bits[idx]
+                end
+                idx += 1
               end
-              idx += 1
             end
+
+            # Skip the following:
+            # - Keys starting with "_"
+            # - Keys starting with "has_"
+            # - Keys ending in "_count" or "_size"
+            if key.index("_") == 0 or key.index("has_") == 0 or key.include? "_bits"
+              next
+            end
+
+            # Convert ip_address_bytes to IP address string
+            if key.include? "ip_address_bytes"
+              key = "ip_address"
+              val = val.join(".")
+            end
+
+            data_hash[key.to_sym] = val
+
           end
 
-          # Skip the following:
-          # - Keys starting with "_"
-          # - Keys starting with "has_"
-          # - Keys ending in "_count" or "_size"
-          if key.index("_") == 0 or key.index("has_") == 0 or key.include? "_bits"
-            next
-          end
-
-          # Convert ip_address_bytes to IP address string
-          if key.include? "ip_address_bytes"
-            key = "ip_address"
-            val = val.join(".")
-          end
-
-
-          data_hash[key.to_sym] = val
-
+          cmd_hash[:data] = data_hash
         end
-
-        cmd_hash[:data] = data_hash
       end
     end
 
